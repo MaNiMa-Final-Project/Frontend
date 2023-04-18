ecc664b7fad6b242bc5ff74866b70487cd28a623
NinaA3000 <106072285+NinaA3000@users.noreply.github.com>
2023-04-18 12:41:47 +0200

    changed header and design, social media button, added course text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,36 @@
+import React from "react";
+import SocialFollow from "./SocialFollow"
+import {
+    faYoutube,
+    faFacebook,
+    faTwitter,
+    faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+  return (
+    <div class="social-container">
+      <h3>Follow us</h3>
+      <a href="https://www.youtube.com/c/jamesqquick"
+        className="youtube social">
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a href="https://www.facebook.com/learnbuildteach/"
+        className="facebook social">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/learnbuildteach"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+      <SocialFollow />
+    </div>
+  );
+}
+
 
 
 export default function Footer(){
--- src/service/fakeData.js
+++ src/service/fakeData.js
@@ -1,51 +1,61 @@
 export const data = [
     {    
         _id: 1,
-        title: "Farblehre",
+        title: "Zeichenkurs Grundlagen I",
         creator: "Nina Ansari",
         beginning: "14.04.2023",
 
         start: { type: String, required: true},
         end: { type: String, required: true},
 
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident tempore amet quas ducimus consectetur tenetur distinctio hic facere laboriosam iure temporibus, quam necessitatibus placeat cum blanditiis numquam illum esse saepe eos! Deserunt similique maiores saepe eum eveniet illum numquam ipsam labore libero, temporibus mollitia adipisci reiciendis commodi animi, qui itaque."
+        description: "• die Grundlagen des Zeichnens verstehen • die richtigen Materialien kennenlernen und lernen, sie richtig einzusetzen • die neue Technik auf andere Elemente des Zeichnens zu übertragen • die eigene Kreativität entdecken • vom Alltag abzuschalten FÜR WEN EIGNET SICH DIESER KURS?"
     },
     {
         _id: 2,
-        title: "Design",
+        title: "Zeichenkurs Grundlagen II",
         creator: "Nina Ansari",
         beginning: "14.04.2023",
         start: { type: String, required: true},
         end: { type: String, required: true},
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id exercitationem ipsam quod accusamus quasi praesentium deserunt magnam! Mollitia optio minus debitis accusantium dolorum culpa rerum, dignissimos minus, non consectetur, dolor saepe fugit quod qui molestias dolorem quos suscipit."
+        description: "• die Grundlagen des Zeichnens verstehen • die richtigen Materialien kennenlernen und lernen, sie richtig einzusetzen • die neue Technik auf andere Elemente des Zeichnens zu übertragen • die eigene Kreativität entdecken • vom Alltag abzuschalten FÜR WEN EIGNET SICH DIESER KURS?"
     },
     {
         _id: 3,
-        title: "Kolibries malen",
+        title: "Portfolio I / Bewerbungsmappe I",
         creator: "Nina Ansari",
         beginning: "14.04.2023",
         start: { type: String, required: true},
         end: { type: String, required: true},
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id exercitationem ipsam quod accusamus quasi praesentium deserunt magnam! Mollitia optio minus tenetur illum eaque! Totam autem iste ipsum debitis accusantium dolorum culpa rerum, dignissimos minus, non consectetur, dolor saepe fugit quod qui molestias dolorem quos suscipit."
+        description: "•	Erstellung von Layouts mit Text und Bild für Webseite, Portfolio, Publikation, Künstlerbuch oder Ausstellung im Raum • Reflexion der eigenen Arbeiten und im Team • Prozessdefinierung in Phasen •	Training des zeitökonomischen Arbeitens • PR im Social Media • Wissen über die kommunikativen Aufgaben und Entwicklung von Webanwendungen •	Regeln und Leitsätze der erfolgreichen, branchenadäquaten Selbstdarstellung FÜR WEN EIGNET SICH DIESER KURS?"
     },
     {    
         _id: 4,
-        title: "Farblehre",
+        title: "Portfolio I / Bewerbungsmappe II",
         creator: "Nina Ansari",
         beginning: "14.04.2023",
 
         start: { type: String, required: true},
         end: { type: String, required: true},
 
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident tempore amet quas ducimus consectetur tenetur distinctio hic facere laboriosam iure temporibus, quam necessitatibus placeat cum blanditiis numquam illum esse saepe eos! Deserunt similique maiores saepe eum eveniet illum numquam ipsam labore libero, temporibus mollitia adipisci reiciendis commodi animi, qui itaque."
+        description: "•	Erstellung von Layouts mit Text und Bild für Webseite, Portfolio, Publikation, Künstlerbuch oder Ausstellung im Raum • Reflexion der eigenen Arbeiten und im Team • Prozessdefinierung in Phasen •	Training des zeitökonomischen Arbeitens • PR im Social Media • Wissen über die kommunikativen Aufgaben und Entwicklung von Webanwendungen •	Regeln und Leitsätze der erfolgreichen, branchenadäquaten Selbstdarstellung FÜR WEN EIGNET SICH DIESER KURS?"
     },
     {
         _id: 5,
-        title: "Design",
+        title: "Fotografie Ideenfindung Grundkurs I",
+        creator: "Nina Ansari",
+        beginning: "14.04.2023",
+        start: { type: String, required: true},
+        end: { type: String, required: true},
+        description: "•	Vermittlung professioneller Fotografie: künstlerische Aussagen zu verstehen und fotografische Konzepte zu entwickeln • Beherrschen von Kameraequipment und Fine-Art-Printing •	Digitale Bildbearbeitung und Fotoarchivierung zu optimieren FÜR WEN EIGNET SICH DIESER KURS? "
+    }
+
+/*     {
+        _id: 6,
+        title: "Fotografie Ideenfindung Grundkurs II",
         creator: "Nina Ansari",
         beginning: "14.04.2023",
         start: { type: String, required: true},
         end: { type: String, required: true},
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id exercitationem ipsam quod accusamus quasi praesentium deserunt magnam! Mollitia optio minus debitis accusantium dolorum culpa rerum, dignissimos minus, non consectetur, dolor saepe fugit quod qui molestias dolorem quos suscipit."
-    }
+    } */
 ]
