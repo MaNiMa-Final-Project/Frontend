6f74bddb36b0c6696b9314b1024154abf053faa6
NinaA3000 <106072285+NinaA3000@users.noreply.github.com>
2023-04-21 16:36:07 +0200

    finised social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,50 @@
+
 import React from "react";
-// import SocialFollow from "./SocialFollow"
-// import {
-//     faYoutube,
-//     faFacebook,
-//     faTwitter,
-//     faInstagram
-//   } from "@fortawesome/free-brands-svg-icons";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 
+export default function Footer() {
 
+  return (
+    <div className="footer">
+      <p>AGB</p>
+      <p>FAQ</p>
+      <p>Nutzungsbedingungen</p>
+      <p>Datenschutzerklärung</p>
+      <p>Cookie-Richtlinie</p>
+      <p>Impressum</p> 
+      <p>© 2023 VG Bildkunst</p> 
 
-export default function Footer(){
-
-    return(
-        <div className="footer">
-            <p>Nutzungsbedingungen</p>
-            <p>Datenschutzerklärung</p>
-            <p>Cookie-Richtlinie</p>
-
-            {/* <div class="social-container">
-              <h3>Follow us</h3>
-              <a href="https://www.youtube.com/c/jamesqquick"
-                className="youtube social">
-                <FontAwesomeIcon icon={faYoutube} size="2x" />
-              </a>
-              <a href="https://www.facebook.com/learnbuildteach/"
-                className="facebook social">
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </a>
-              <a href="https://www.twitter.com/jamesqquick" className="twitter social">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-              </a>
-              <a href="https://www.instagram.com/learnbuildteach"
-                className="instagram social">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </a>
-              <SocialFollow />
-            </div> */}
-        </div>
-    )
+      {<div class="social-container">
+        <p>Follow us</p>
+        <a href="https://www.youtube.com/c/jamesqquick" target="_blank"
+          className="youtube social">
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a href="https://www.facebook.com/learnbuildteach/" target="_blank"
+          className="facebook social">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="https://www.twitter.com/jamesqquick" target="_blank" className="twitter social">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a href="https://www.instagram.com/learnbuildteach" target="_blank"
+          className="instagram social">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href ="https://www.linkedin.com/company/studioartshuttle/?viewAsMember=true" target="_blank"
+          className="linkedin social">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+      </div>}
+    </div>
+  )
 }
