85ebaf5663b469aa9cba64a9b1f331c8168bf745
Martin <martindrus@outlook.de>
2023-04-22 10:33:39 +0200

    centered footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,16 +15,17 @@ export default function Footer() {
 
   return (
     <div className="footer">
-      <p>AGB</p>
-      <p>FAQ</p>
-      <p>Nutzungsbedingungen</p>
-      <p>Datenschutzerklärung</p>
-      <p>Cookie-Richtlinie</p>
-      <p>Impressum</p> 
-      <p>© 2023 VG Bildkunst</p> 
+      <div className="shortcuts">
+        <p>AGB</p>
+        <p>FAQ</p>
+        <p>Nutzungsbedingungen</p>
+        <p>Datenschutzerklärung</p>
+        <p>Cookie-Richtlinie</p>
+        <p>Impressum</p> 
+        <p>© 2023 VG Bildkunst</p> 
+      </div>
 
-      {<div class="social-container">
-        <p>Follow us</p>
+      <div className="social-container">
         <a href="https://www.youtube.com/c/jamesqquick" target="_blank"
           className="youtube social">
           <FontAwesomeIcon icon={faYoutube} size="2x" />
@@ -44,7 +45,7 @@ export default function Footer() {
           className="linkedin social">
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
-      </div>}
+      </div>
     </div>
   )
 }
