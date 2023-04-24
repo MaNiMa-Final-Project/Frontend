73c544476de74a497796f4574686cb6da92cf103
Martin <martindrus@outlook.de>
2023-04-24 10:39:12 +0200

    total hinzugefügt

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useCartData } from '../hooks/useCartData'
-import '../components/ShoppingCart/shoppingCart.scss'
+import '../components/ShoppingCart/shoppingCart.scss';
+
+import { faSquareXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import axios from 'axios';
 import { TEMP_URL_COURSE } from "../service/config";
 
 export default function ShoppingCartPage(){
     const cartData = useCartData();
-    const [courses, setCourses] = useState([])
+    const [courses, setCourses] = useState([]);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         (async () => {
@@ -26,25 +30,30 @@ export default function ShoppingCartPage(){
         cartData.removeFromCart(courseId)
     }
 
+    let subTotal = 0;
     let cartItem = courses.map(course => {
-        //todo insert delete icon
+        
 
         let temp = new Date(course.beginning).toLocaleString('de-DE').split(',')
         let date = temp[0]+` - ${course.start} Uhr`
 
+
         return(
             <tr key={course._id} className='tableRow'>
                 
-                <td className='tableDataCell'><button onClick={() => handleCartDelete(course._id)} >delete</button></td>
+                <td className='tableDataCell'><button onClick={() => handleCartDelete(course._id)} ><FontAwesomeIcon icon={faSquareXmark} /></button></td>
                 <td className='tableDataCell'>{course.title}</td>
-                <td className='tableDataCell'>{course.price} €</td>
 
                 <td className='tableDataCell'>{date}</td>
                 <td className='tableDataCell'>{course.duration}</td>
-                <td className='tableDataCell'>{course.start}</td>
+                <td className='tableDataCell'>{course.price} €</td>
+                <td className='tableDataCell'>{}</td>
+
+                <td className='tableDataCell'>{subTotal+= course.price} €</td>
 
             </tr>
         )
+        
     })
 
 
@@ -52,16 +61,21 @@ export default function ShoppingCartPage(){
     return(
 
         <div className='cartContainer'>
-            <h1>ShoppingCart</h1>
 
             <table>
                 <thead>
                     <tr>
                         <th></th>
                         <th>Course</th>
-                        <th>Price</th>
                         <th>Date</th>
+
                         <th>Duration</th>
+
+                        <th>Price</th>
+                        <th></th>
+                        <th>Subtotal</th>
+
+
 
                     </tr>
                 </thead>
@@ -72,7 +86,18 @@ export default function ShoppingCartPage(){
 
                 </tfoot>
             </table>
-
+            <div className='cartSummary'>
+                <div className='cartTotal'>
+                    <div className='cartSummaryHeader'>
+                        <h1>Cart totals</h1>
+                    </div>
+                    <div className='cartSummaryBody'>
+                        <hr />
+                        <h2>Total</h2>
+                        {subTotal}
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
