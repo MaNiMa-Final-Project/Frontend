d05d15ff9ed626137ed2cf6dd29a99a900b6021d
Martin <martindrus@outlook.de>
2023-04-25 12:07:23 +0200

    added role change for users

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,22 +1,160 @@
+import { useState } from 'react'
+import axios from 'axios'
 import './dashboard.scss'
+import { TEMP_URL_COURSE, BASE_URL_PROTECTED } from '../../service/config';
 
 export default function AdminDashboard() {
 
+  const [userResult, setUserResult] = useState([]);
+  const [courseResult, setCourseResult] = useState([]);
 
-    return(
-        <div className="adminViewContainer">
-            <div className="userSection">
+  const [userQuery, setUserQuery] = useState('');
+  const [courseQuery, setCourseQuery] = useState('');
 
-            <form>
-                <input type="text" />
-                <button>Suche</button>
+  const [selectedValue, setSelectedValue] = useState("");
+
+
+  const handleFormSubmit = async (event, userId) => {
+    event.preventDefault();
+    if(selectedValue && userId){
+      try {
+        const response = await axios.get(BASE_URL_PROTECTED+`role?u=${userId}&r=${selectedValue}`, {
+          withCredentials: true
+        });
+
+        handleUserSearch(event)
+      } catch(error) {
+        // Fehlerbehandlung bei Anfrageproblemen
+        console.error('Fehler bei der Benutzeranfrage:', error);
+      }
+    }
+
+
+  };
+
+  const handleSelectChange = (event) => setSelectedValue(event.target.value);
+  
+  
+
+  // Die handleUserSearch-Funktion
+  const handleUserSearch = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await axios.get(BASE_URL_PROTECTED+`users?q=${userQuery}`, {
+        withCredentials: true
+      });
+      setCourseResult([]);
+      setUserResult(response.data);
+    } catch(error) {
+      // Fehlerbehandlung bei Anfrageproblemen
+      console.error('Fehler bei der Benutzeranfrage:', error);
+    }
+
+  }
+
+  // Die handleCourseSearch-Funktion
+  const handleCourseSearch = async (event) => {
+    event.preventDefault();
+      try {
+        const response = await axios.get(TEMP_URL_COURSE+`course?q=${courseQuery}`, {
+          withCredentials: true
+        });
+        setUserResult([]);
+        setCourseResult(response.data);
+      } catch(error) {
+        // Fehlerbehandlung bei Anfrageproblemen
+        console.error('Fehler bei der Kursanfrage:', error);
+      }
+    }
+
+
+
+
+
+    let courses = courseResult.map((course, index) => {
+      let dozent = course.creator
+
+      return <li key={course._id}>
+        <span>
+          {index+1}. 
+          <p>Kursname: {course.title}</p>
+          <p>Dozent: {dozent.nickName}</p>
+        </span>
+        <button>edit</button>
+
+
+      </li>
+    })
+
+
+    let users = userResult.map((user, index) => {
+      const roleObj = user.role;
+      const role = roleObj.name;
+      const capitalizedString = role.charAt(0).toUpperCase() + role.slice(1);
+
+        return <li key={user._id}>
+            <span>
+              {index+1}. 
+              <p className='role'>Rolle: {capitalizedString}</p>
+              <p>Nutzername: {user.nickName}</p>
+              <p>Vorname: {user.firstName}</p>
+              <p>Nachname: {user.lastName}</p>
+            </span>
+            <form onSubmit={(e) => handleFormSubmit(e, user._id)}>
+              <select value={selectedValue} onChange={handleSelectChange}>
+                <option value="">Rollen</option>
+                {capitalizedString != 'User' && <option value="6405ac7d6b2564cd76c42605">User</option>}
+                {capitalizedString != 'Creator' && <option value="6405ac7d6b2564cd76c42604">Creator</option>}
+                {capitalizedString != 'Admin' && <option value="6405ac7d6b2564cd76c42603">Admin</option>}
+              </select>
+              <button type="submit">Submit</button>
             </form>
+            <button>anonymize</button>
+            <button>delete</button>
 
-            </div>
+        </li>
+    })
 
-            <div className="courseSection">
 
-            </div>
-        </div>
-    )
+  return (
+    <div className="adminViewContainer">
+      <div className="userSection">
+        <form onSubmit={handleUserSearch}>
+          <input 
+            type="text"
+            value={userQuery} 
+            onChange={(event) => setUserQuery(event.target.value)}
+          />
+          <button type="submit">Suche User</button>
+        </form>
+
+
+      </div>
+
+      <div className="courseSection">
+        <form onSubmit={handleCourseSearch}>
+          <input 
+            type="text" 
+            value={courseQuery} 
+            onChange={(event) => setCourseQuery(event.target.value)}
+          />
+          <button type="submit" >Suche Kurs</button>
+        </form>
+
+
+      </div>
+
+      <div className="resultSection">
+        <h1>Suchergebnis
+              {courseResult.length > 0 && ' für Kurse'}
+              {userResult.length > 0 && ' für User'}
+        </h1>
+          <ul className='resultList'>
+              {courseResult && courses}
+              {userResult && users}
+          </ul>
+      </div>
+    </div>
+  )
 }
